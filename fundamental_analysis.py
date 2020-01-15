89b3e4eca72437742fcc827eb4b74fcd00280686
Duc Cao <duccao1989@gmail.com>
2020-01-15 10:35:23 +0100

    Remove boilerplate code

--- fundamental_analysis.py
+++ fundamental_analysis.py
@@ -1,14 +1,25 @@
 from unibit_api import financial_report
 
 
+UNKNOWN_VALUE = '-'
+
+
 def string_to_number(s):
-    return float(s.replace(',', ''))
+    try:
+        return float(s.replace(',', ''))
+    except:
+        return UNKNOWN_VALUE
 
 
 def dict_reports(financial_report, interval='annual'):
+    def _to_float(dictionary):
+        "Convert all values of a dictionary to float data type"
+        return {key: string_to_number(value) \
+                for key, value in dictionary.items()}
+
     reports = list(financial_report['result_data'].values())[0]
     # TODO extract month and year from reportDate is interval is quarterly
-    return {report['reportDate'].split('/')[-1]: report for \
+    return {report['reportDate'].split('/')[-1]: _to_float(report) for \
             report in reports}
 
 
@@ -42,10 +53,10 @@ def dividend_to_net_income(ticker, interval='annual'):
         periods = comparable_periods(dict_ic, dict_cf)
         for period in periods:
             try:
-                ratio = abs(string_to_number(dict_cf[period]['dividendsPaid']) / \
-                           string_to_number(dict_ic[period]['netIncome']))
+                ratio = abs(dict_cf[period]['dividendsPaid'] / \
+                           dict_ic[period]['netIncome'])
             except:
-                ratio = '-'
+                ratio = UNKNOWN_VALUE
             yield {period: ratio}
 
     return compute_ratios(ticker, _ratio, interval)
