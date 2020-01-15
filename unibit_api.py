bfb9dce2e1c822116ab858d60365a85d1139ad03
Duc Cao <duccao1989@gmail.com>
2020-01-15 17:34:20 +0100

    Download company profiles to filter them by sector/industry

--- unibit_api.py
+++ unibit_api.py
@@ -5,8 +5,12 @@ from datetime import datetime
 
 
 ACCESS_KEY = open('api_key.config').read().strip()
-API_ENDPOINT = 'https://api.unibit.ai/v2/company/financials?'
+API_ENDPOINT = 'https://api.unibit.ai/v2'
+FINANCIAL_API = f'{API_ENDPOINT}/company/financials?'
+COMPANIES_API = f'{API_ENDPOINT}/ref/companyList?'
+PROFILE_API = f'{API_ENDPOINT}/company/profile?'
 CACHE_FOLDER = 'cache'
+COMPANIES_CSV = CACHE_FOLDER + '/exchange_{exchange}.csv'
 if not os.path.exists(CACHE_FOLDER):
     os.mkdir(CACHE_FOLDER)
 
@@ -21,7 +25,7 @@ def financial_report(ticker, statement, interval='annual'):
         with open(ticker_cache_file) as f:
             _json = f.read()
     else:
-        r = requests.get(API_ENDPOINT, params={
+        r = requests.get(FINANCIAL_API, params={
             'tickers': ticker,
             'statement': statement,
             'startDate': '2010-01-01',
@@ -35,4 +39,32 @@ def financial_report(ticker, statement, interval='annual'):
                out.write(_json)
 
     return json.loads(_json)
-    
+
+
+def list_companies(exchange):
+    r = requests.get(COMPANIES_API, params={
+        'exchange': exchange,
+        'accessKey': ACCESS_KEY
+    })
+    if r.status_code == 200:
+        return r.json()
+
+
+def download_company_profiles(exchange):
+    companies = list_companies(exchange)
+    tickers = [item['ticker'] for item in companies['result_data']]
+
+    def divide_chunks(_list, chunk_size):
+        for i in range(0, len(_list), chunk_size):
+            yield _list[i:i + chunk_size]
+
+    for sub_tickers in divide_chunks(tickers, chunk_size=50):
+        r = requests.get(PROFILE_API, params={
+            'tickers': ','.join(sub_tickers),
+            'dataType': 'csv',
+            'accessKey': ACCESS_KEY
+        })
+        print(r.status_code)
+        if r.status_code == 200:
+            with open(COMPANIES_CSV.format(exchange=exchange), 'a+') as out:
+                out.write(r.text)
